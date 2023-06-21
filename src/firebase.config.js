9817d62bd70d017e4f2e9c8cc6490e8c41ec7d4e
Selim Sami <Selim.Genc@hhu.de>
2023-06-21 10:20:17 +0200

    added some pages

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_API_KEY}`,
-  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
-  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.REACT_APP_APP_ID}`
+  apiKey: `AIzaSyBzchJNACW3QzMFZva-WcBvhiJMnTSYNNw`,
+  authDomain: `house-market-place-9200d.firebaseapp.com`,
+  projectId: `house-market-place-9200d`,
+  storageBucket: `house-market-place-9200d.appspot.com`,
+  messagingSenderId: `117937877892`,
+  appId: `1:117937877892:web:0c31504c2b9fda789ae3d1`
 };
 
 // Initialize Firebase
